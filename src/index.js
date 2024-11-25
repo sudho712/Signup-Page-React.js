80a83ad6c3b8d11a8616dc736215a9cf8471ea67
Sudhir Kumar <sudho712@gmail.com>
2024-11-25 21:42:24 +0530

    twitter signup page

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If needed, uncomment the following line to log performance metrics:
+// import reportWebVitals from './reportWebVitals';
+// reportWebVitals(console.log);
