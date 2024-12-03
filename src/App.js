037ba0ff7902d40337b48c367cc3aa544f84c97d
Sudhir Kumar <sudho712@gmail.com>
2024-12-03 23:58:06 +0530

    twitter sign up page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ function App() {
       <div className="logo-box">
         <img src="./twitter.png" alt="Twitter logo" />
         <img src="../twitter/logo/apple.png" alt="Apple logo" />
-        
+        <h2>Sign In Twitter</h2>
+        <button>
+          <img src="./google.png" alt="" />
+          Sign in with  google
+        </button>
+        <button>
+          <img src="./apple.png" alt="" />
+          Sign in with  google
+        </button>
+       <hr></hr><span>Or</span>
+       <form>
+        <input type="text" placeholder="Phone or username" />
+        <button>Next</button>
+       </form>
+       
+       <button>Forget Password</button>
+       <p>Don't Have account <a>Signup</a> </p>
       </div>
     </div>
   );
