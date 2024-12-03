b2cdf9580e6fd9f16923771fd4a4c6566ee292e7
Sudhir Kumar <sudho712@gmail.com>
2024-12-03 23:27:41 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div classname="App">
-      <div className='logo-box'>
-        <img src="./twitter.png" alt="logo" />
+    <div className="App">
+      <div className="logo-box">
+        <img src="./twitter.png" alt="Twitter logo" />
+        <img src="../twitter/logo/apple.png" alt="Apple logo" />
+        
       </div>
-    </div>    
+    </div>
   );
 }
 
